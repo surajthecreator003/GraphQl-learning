153adb58d9e76f1e563048f92ff1ab9b90c2400c
Suraj Mallick <surajhustlermallick@gmail.com>
2024-01-01 19:57:25 +0530

    added frontend and configured with apollo inside react app and also did git reset  --hard  HEAD^  and git push origin HEAD --force to update missuploaded

--- server/server.js
+++ server/server.js
@@ -1,42 +1,60 @@
 const express = require("express");
-const {ApolloServer}=require("@apollo/server");
-const {expressMiddleware}=require("@apollo/server/express4")
-const bodyParser=require("body-parser");
-const cors=require("cors");
-const {default: axios}=require("axios");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
 
+const { USERS } = require("./user");
+const { TODOS } = require("./todo");
 
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+        type User {
+            id: ID!
+            name: String!
+            username: String!
+            email: String!
+            phone: String!
+            website: String!
+        }
 
-async function startServer(){
-    const app=express();
-    const server=new ApolloServer({
-        typeDefs:`type Todo{
-            userId:Int
+        type Todo {
             id: ID!
             title: String!
-            completed: Boolean           
+            completed: Boolean
+            user: User
         }
-        
+
         type Query {
-                 getTodos:[Todo]
+            getTodos: [Todo]
+            getAllUsers: [User]
+            getUser(id: ID!): User
         }
 
-        `,
-        resolvers:{
-            Query:{
-                getTodos:async()=>(await axios.get("https://jsonplaceholder.typicode.com/todos")).data
-            }
-        }
-    });
+    `,
+    resolvers: {
+      Todo: {
+        user: (todo) => USERS.find((e) => e.id === todo.id),
+      },
+      Query: {
+        getTodos: () => TODOS,
+        getAllUsers: () => USERS,
+        getUser: async (parent, { id }) => USERS.find((e) => e.id === id),
+      },
+    },
+  });
 
-    app.use(bodyParser.json());
-    app.use(cors());
+  app.use(bodyParser.json());
+  app.use(cors());
 
-    await server.start();
+  await server.start();
 
-    app.use("/graphql",expressMiddleware(server))
+  app.use("/graphql", expressMiddleware(server));
 
-    app.listen(8051,()=>{console.log("Server Started")})
+  app.listen(8011, () => console.log("Serevr Started at PORT 80110"));
 }
 
 startServer();
